c906a62e7361364b5b8cbac1c3eed3eb5514a9d3
zitangerine <91669344+zitangerine1@users.noreply.github.com>
2022-02-28 21:27:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-function poof() {
-  document.querySelector(".tempBG").style.opacity = 0;
-  document.querySelector(".popup").style.opacity = 0;
-  document.querySelector(".stop").style.opacity = 0;
-};
-
 const popup = document.querySelector(".popup")
 const bg = document.querySelector(".tempBG")
 const stop = document.querySelector(".stop")
+
+function poof() {
+  bg.style.opacity = 0;
+  popup.style.opacity = 0;
+  stop.style.opacity = 0;
+};
+
+function poof2() {
+  bg.style.zIndex = 1;
+}
 
 document.querySelector("#close").onclick = function () {
   popup.classList.toggle('fade');
