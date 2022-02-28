3c217ad3d4f6a9d403bdb9205276552a226e7757
zitangerine <91669344+zitangerine1@users.noreply.github.com>
2022-02-28 20:46:44 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,3 +3,11 @@ function poof() {
   document.querySelector(".popup").style.opacity = 0;
   document.querySelector(".stop").style.opacity = 0;
 };
+
+const popup = document.querySelector(".popup")
+const bg = document.querySelector(".tempBG")
+
+document.querySelector("#close").onclick = function () {
+  popup.classList.toggle('fade');
+  bg.classList.toggle('fade');
+}`
